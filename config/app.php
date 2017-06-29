042ba7b17ba06eaa5a8530a4f478f9d10fde81f5
Christian Graf <christian.graf@cyberfox.at>
2017-06-29 17:06:24 +0200

    chore(composer): require yab/quarx ^2.3

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
+         * Laravel Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
 
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /**
+         * 3rd Party Service Providers...
+         */
+        Yab\Quarx\QuarxProvider::class,
     ],
 
     /*
